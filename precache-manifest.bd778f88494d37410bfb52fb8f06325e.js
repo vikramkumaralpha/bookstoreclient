5e37423d8721d4f833978972c713c4d54f57eb28
vikramkumaralpha <91052715+vikramkumaralpha@users.noreply.github.com>
2021-09-23 22:42:31 +0530

    Updates

---
+++ precache-manifest.bd778f88494d37410bfb52fb8f06325e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea5fe898cfae1f4b656dd13c0e9aafbd",
+    "url": "/bookstoreclient/index.html"
+  },
+  {
+    "revision": "597aac4f4e4a2117450f",
+    "url": "/bookstoreclient/static/css/main.4f353394.chunk.css"
+  },
+  {
+    "revision": "907b983b35b5fc9e51eb",
+    "url": "/bookstoreclient/static/js/2.5ff88e95.chunk.js"
+  },
+  {
+    "revision": "597aac4f4e4a2117450f",
+    "url": "/bookstoreclient/static/js/main.94891a37.chunk.js"
+  },
+  {
+    "revision": "9c54440861de1bc59647",
+    "url": "/bookstoreclient/static/js/runtime-main.567a14b3.js"
+  }
+]);
